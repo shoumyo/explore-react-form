66fd907065246b16dfb4d4217586c7ab63bddb5a
Shoumyo <shoumyosrijito@gmail.com>
2026-06-09 23:15:22 +0600

    implement product form submission with validation; update product table to display product list

--- src/Components/ProductManagement/ProductForm.jsx
+++ src/Components/ProductManagement/ProductForm.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProductForm = () => {
+const ProductForm = ({handleProducts}) => {
+
+    const[Error,setError]=useState('');
 
     const handleProductSubmit =e=>{
         e.preventDefault();
         const name=e.target.name.value;
         const price=e.target.price.value;
-        const quantity=e.target.quanttity.value;
+        const quantity=e.target.quantity.value;
+
+        if(name.length===0){
+            setError('Provide a name');
+        }
+        else if(price.length===0){
+            setError('Provide a price');
+            return;
+        }
+        else if(price<0){
+            setError('Provide a positive price');
+            return;
+        }
+        else if(quantity.length===0){
+            setError('Provide a quantity');
+            return;
+        }
 
         const newProduct={
             name,
             price,
             quantity
         }
+        handleProducts(newProduct);
     }
 
     return (
@@ -27,6 +46,7 @@ const ProductForm = () => {
                 <br />
                 <input type="submit" value="submit" />
             </form>
+            <p style={{color:'red'}}><small>{Error}</small></p>
         </div>
     );
 };
--- src/Components/ProductManagement/ProductManagement.jsx
+++ src/Components/ProductManagement/ProductManagement.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductForm from './ProductForm';
 import ProductTable from './ProductTable';
 
 const ProductManagement = () => {
+
+    const[products,setProducts]=useState([]);
+
+    const handleProducts=newProduct=>{
+        const newProducts=[...products,newProduct];
+        setProducts(newProducts);
+    }
+
     return (
         <div>
-            <ProductForm></ProductForm>
-            <ProductTable></ProductTable>
+            <ProductForm handleProducts={handleProducts}></ProductForm>
+            <ProductTable products={products}></ProductTable>
         </div>
     );
 };
